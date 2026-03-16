d556a0cf008002dd60f61c55b997a4e49c502bce
sychoi <cinnamon1995@naver.com>
2026-03-16 18:26:13 +0900

    :sparkles: feat: 라우터 그룹 분리 및 사이드바 레이아웃 추가

---
+++ src/app/(routers)/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '슬리드 투두',
+  description:
+    '슬리드 투두는 다양한 콘텐츠를 할 일 목록으로 관리하고 학습 진도, 프로젝트 진행 상황 등을 대시보드로 보여주며, 각 할일에 대한 노트를 작성해 관리해주는 서비스입니다.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="h-dvh w-full bg-gray-100 md:flex">
+      <div className="flex h-14 w-full bg-white md:h-full md:w-[60px] md:rounded-r-2xl lg:w-[362px]" />
+      <div className="flex h-full w-full items-center justify-center bg-gray-100">{children}</div>
+    </div>
+  );
+}
