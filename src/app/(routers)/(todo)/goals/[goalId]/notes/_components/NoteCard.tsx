938f22e5095521dfcffbe742a6dd063310c4b5bf
sychoi <cinnamon1995@naver.com>
2026-03-23 01:58:46 +0900

    :bug: fix: 부모단에서 전달되는 이벤트 전파 막아주는 처리변경

--- src/app/(routers)/(todo)/goals/[goalId]/notes/_components/NoteCard.tsx
+++ src/app/(routers)/(todo)/goals/[goalId]/notes/_components/NoteCard.tsx
@@ -54,63 +54,56 @@ export default function NoteCard({ id, title, todoTitle, isDone, updatedAt }: No
           <p className="font-sm-semibold md:font-xl-semibold line-clamp-2 text-gray-800">{title}</p>
         </div>
         {/*컴포넌트로 빼기*/}
-        <Dialog>
-          <DialogTrigger
-            render={
-              <Button
-                variant="icon"
-                size="none"
-                onClick={(e) => {
-                  e.stopPropagation(); // 부모로 전파 중지
-                  e.preventDefault(); // 링크 이동 중지
-                  console.log('클릭됨');
-                }}
-                className="hover:bg-orange-800"
-                aria-label="더보기"
-              >
-                <Icon name="dotscircle" size={24} />
-              </Button>
-            }
-          />
-          <DialogContent
-            showCloseButton={false}
-            className="w-86 space-y-8 pt-8 md:w-114 md:space-y-10 md:pt-8"
-          >
-            <DialogHeader>
-              <DialogTitle>정말 삭제하시겠어요?</DialogTitle>
-              <DialogDescription className="font-xs-regular md:font-base-regular flex items-center text-orange-600">
-                <Icon name={'warning'} variant={'orange'} size={18} />
-                삭제된 노트는 복구할 수 없습니다.
-              </DialogDescription>
-            </DialogHeader>
-            <DialogFooter>
-              <DialogClose
-                render={
-                  <Button type="button" variant="ghost" className="w-1/2">
-                    취소
-                  </Button>
-                }
-              />
-              <DialogClose
-                render={
-                  <Button
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      console.log('delete note');
-                    }}
-                    type="button"
-                    variant="default"
-                    className="w-1/2"
-                  >
-                    확인
-                  </Button>
-                }
-              />
-            </DialogFooter>
-          </DialogContent>
-        </Dialog>
+        <div
+          onClick={(e) => {
+            e.stopPropagation(); // 부모로 전파 중지
+            e.preventDefault(); // 링크 이동 중지
+          }}
+        >
+          <Dialog>
+            <DialogTrigger
+              render={
+                <Button
+                  variant="icon"
+                  size="none"
+                  className="hover:bg-orange-800"
+                  aria-label="더보기"
+                >
+                  <Icon name="dotscircle" size={24} />
+                </Button>
+              }
+            />
+            <DialogContent
+              showCloseButton={false}
+              className="w-86 space-y-8 pt-8 md:w-114 md:space-y-10 md:pt-8"
+            >
+              <DialogHeader>
+                <DialogTitle>정말 삭제하시겠어요?</DialogTitle>
+                <DialogDescription className="font-xs-regular md:font-base-regular flex items-center text-orange-600">
+                  <Icon name={'warning'} variant={'orange'} size={18} />
+                  삭제된 노트는 복구할 수 없습니다.
+                </DialogDescription>
+              </DialogHeader>
+              <DialogFooter>
+                <DialogClose
+                  render={
+                    <Button type="button" variant="ghost" className="w-1/2">
+                      취소
+                    </Button>
+                  }
+                />
+                <DialogClose
+                  render={
+                    <Button type="button" variant="default" className="w-1/2">
+                      확인
+                    </Button>
+                  }
+                />
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+        </div>
       </div>
-
       {/* 하단: 뱃지 + 할일 제목 + 날짜 */}
       <div className="flex h-5 items-center justify-between md:h-5.5">
         <div className="flex items-center gap-1.5 overflow-hidden">
