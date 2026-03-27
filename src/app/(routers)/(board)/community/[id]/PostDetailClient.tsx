d118edede4053b11443f329ee6e435047ce9fc32
suwojiii <bigbanggoodz@gmail.com>
2026-03-27 12:42:30 +0900

    🐛 fix: 소통게시판 에러 화면 교체 방지

--- src/app/(routers)/(board)/community/[id]/PostDetailClient.tsx
+++ src/app/(routers)/(board)/community/[id]/PostDetailClient.tsx
@@ -68,7 +68,7 @@ export function PostDetailClient({ postId }: PostDetailClientProps) {
     }
   }, [editor, post]);
 
-  if (isError || isCommentsError)
+  if ((isError && !post) || (isCommentsError && !comments))
     return (
       <PostErrorFallback
         onRetry={() => {
