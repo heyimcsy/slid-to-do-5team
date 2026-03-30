15110e928bb799179870988e924487d61e548e16
Seunggi Jo <jojosg@nate.com>
2026-03-30 13:20:21 +0900

    flx: vercel build error 수정

--- src/app/(routers)/(todo)/goals/[goalId]/todos/[todoId]/edit/page.tsx
+++ src/app/(routers)/(todo)/goals/[goalId]/todos/[todoId]/edit/page.tsx
@@ -1,13 +1,3 @@
-import Link from 'next/link';
-import { mockTodo } from '@/app/(routers)/(todo)/@modal/(.)goals/[goalId]/todos/[todoId]/edit/page';
-
 export default function EditPage() {
-  const goalId = mockTodo.goalId;
-  const todoId = mockTodo.id;
-
-  return (
-    <div>
-      <Link href={`/goals/${goalId}/todos/${todoId}/edit`}>수정</Link>
-    </div>
-  );
+  return <div>EditPage</div>;
 }
