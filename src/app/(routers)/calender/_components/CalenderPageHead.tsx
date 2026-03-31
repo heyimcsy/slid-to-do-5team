4a2414edded003dd27866f46f59bdbc0e025acc8
sychoi <cinnamon1995@naver.com>
2026-03-31 09:13:31 +0900

    :sparkles: feature: calender 할일 생성 버튼에 링크 추가

--- src/app/(routers)/calender/_components/CalenderPageHead.tsx
+++ src/app/(routers)/calender/_components/CalenderPageHead.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import Link from 'next/link';
+
 import { Icon } from '@/components/icon/Icon';
 import { IconButton } from '@/components/ui/button';
 
@@ -17,9 +19,11 @@ export default function CalenderPageHead() {
   return (
     <div className="hidden w-full items-center justify-between md:flex">
       <h1 className="font-xl-semibold lg:font-2xl-semibold">{userName}님의 캘린더</h1>
-      <IconButton variant="outline">
-        <Icon name="plus" variant="orange" />할 일 추가
-      </IconButton>
+      <Link href="/goals/todos/new" className="flex h-fit w-fit">
+        <IconButton variant="outline">
+          <Icon name="plus" variant="orange" />할 일 추가
+        </IconButton>
+      </Link>
     </div>
   );
 }
