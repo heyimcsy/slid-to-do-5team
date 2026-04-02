3d7bc44b7f28119a6ef686edc39d54e6c822a6ff
sychoi <cinnamon1995@naver.com>
2026-04-02 10:51:51 +0900

    :bug: fix: 노트 작성시 빈값은 저장 안되도록 수정 및 유저가 불러오는 값 직접 삭제시에만 삭제되도록 변경

--- src/app/(routers)/(todo)/goals/[goalId]/notes/new/_components/SpeechComponent.tsx
+++ src/app/(routers)/(todo)/goals/[goalId]/notes/new/_components/SpeechComponent.tsx
@@ -69,7 +69,6 @@ export default function SpeechComponent({
     if (!showDraftBubble || isLoadingDraft) return;
     timerRef.current = setTimeout(() => {
       setShowDraftBubble(false);
-      clearDraft();
     }, 5000);
 
     return () => {
@@ -86,54 +85,67 @@ export default function SpeechComponent({
             <div className="font-sm-medium relative top-8 left-8 z-2 px-5 py-6">
               <p>임시 저장된 노트가 있어요.</p>
               <p>저장된 노트를 불러오시겠어요?</p>
-              <Dialog>
-                <DialogTrigger
+              <div className="flex h-fit w-full space-x-4">
+                <Dialog>
+                  <DialogTrigger
+                    className="font-sm-semibold mt-2 cursor-pointer text-blue-200"
+                    onClick={handleLoadClick}
+                  >
+                    불러오기
+                  </DialogTrigger>
+                  <DialogContent className="max-w-[343px] md:max-w-139" showCloseButton={false}>
+                    <DialogHeader className="mt-8">
+                      <DialogTitle className="text-gray-800">{title}</DialogTitle>
+                      <DialogDescription className="font-sm-semibold md:font-xl-semibold text-gray-800">
+                        제목의 노트를 불러오시겠어요?
+                      </DialogDescription>
+                    </DialogHeader>
+                    <DialogFooter className="mt-10">
+                      <DialogClose
+                        render={
+                          <Button
+                            type="button"
+                            variant="ghost"
+                            className="w-1/2"
+                            onClick={() => {
+                              clearDraft();
+                              setShowDraftBubble(false);
+                            }}
+                          >
+                            취소
+                          </Button>
+                        }
+                      />
+                      <DialogClose
+                        render={
+                          <Button
+                            onClick={() => {
+                              loadDraft();
+                              setShowDraftBubble(false);
+                            }}
+                            type="button"
+                            variant="default"
+                            className="w-1/2"
+                          >
+                            불러오기
+                          </Button>
+                        }
+                      />
+                    </DialogFooter>
+                  </DialogContent>
+                </Dialog>
+                <Button
+                  variant="icon"
+                  size="none"
                   className="font-sm-semibold mt-2 cursor-pointer text-orange-500"
-                  onClick={handleLoadClick}
+                  onClick={() => {
+                    clearDraft();
+                    setShowDraftBubble(false);
+                  }}
                 >
-                  불러오기
-                </DialogTrigger>
-                <DialogContent className="max-w-[343px] md:max-w-139" showCloseButton={false}>
-                  <DialogHeader className="mt-8">
-                    <DialogTitle className="text-gray-800">{title}</DialogTitle>
-                    <DialogDescription className="font-sm-semibold md:font-xl-semibold text-gray-800">
-                      제목의 노트를 불러오시겠어요?
-                    </DialogDescription>
-                  </DialogHeader>
-                  <DialogFooter className="mt-10">
-                    <DialogClose
-                      render={
-                        <Button
-                          type="button"
-                          variant="ghost"
-                          className="w-1/2"
-                          onClick={() => {
-                            clearDraft();
-                            setShowDraftBubble(false);
-                          }}
-                        >
-                          취소
-                        </Button>
-                      }
-                    />
-                    <DialogClose
-                      render={
-                        <Button
-                          onClick={() => {
-                            loadDraft();
-                            setShowDraftBubble(false);
-                          }}
-                          type="button"
-                          variant="default"
-                          className="w-1/2"
-                        >
-                          불러오기
-                        </Button>
-                      }
-                    />
-                  </DialogFooter>
-                </DialogContent>
-              </Dialog>
+                  삭제하기
+                </Button>
+              </div>
             </div>
           </div>
         </div>
