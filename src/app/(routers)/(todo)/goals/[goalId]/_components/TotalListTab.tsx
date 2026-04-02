a2326941661fc2de1576a763cdd089d93d8c2e70
sychoi <cinnamon1995@naver.com>
2026-04-02 14:42:56 +0900

    :bug: fix: 목표 상세 API 조회 구조 변경 및 목표진행도 , 노트 아이콘 바로 적용 되도록 수정

--- src/app/(routers)/(todo)/goals/[goalId]/_components/TotalListTab.tsx
+++ src/app/(routers)/(todo)/goals/[goalId]/_components/TotalListTab.tsx
@@ -1,46 +1,29 @@
 'use client';
 
-import type { TodoWithFavorites } from '@/api/todos';
 import type { TotalListTabProps } from '@/app/(routers)/(todo)/goals/types';
 
-import { useGetTodos } from '@/api/todos';
 import { TodoSection } from '@/app/(routers)/(todo)/goals/[goalId]/_components/TodoSection';
-import { TodoSectionSkeleton } from '@/app/(routers)/(todo)/goals/[goalId]/_components/TodoSectionSkeleton';
 
-export default function TotalListTab({ goalId }: TotalListTabProps) {
-  const { data, isSuccess, isLoading } = useGetTodos({ goalId, limit: 20 });
-
-  const todoLists = data?.todos.filter((todo: TodoWithFavorites) => !todo.done) ?? [];
-  const todoListsDone = data?.todos.filter((todo: TodoWithFavorites) => todo.done) ?? [];
-
-  if (isLoading) {
-    return (
-      <div className="flex flex-col space-y-4 md:space-y-4 lg:flex-row lg:space-x-8">
-        <TodoSectionSkeleton showActions />
-        <TodoSectionSkeleton />
-      </div>
-    );
-  }
-  if (isSuccess)
-    return (
-      <div className="flex flex-col space-y-4 md:space-y-4 lg:flex-row lg:space-x-8">
-        <TodoSection
-          goalId={goalId}
-          title="TO DO"
-          todos={todoLists}
-          bgColor="bg-orange-100"
-          emptyImage="/images/big-zero-todo.svg"
-          emptyText="해야할 일이 아직 없어요"
-          showActions
-        />
-        <TodoSection
-          goalId={goalId}
-          title="DONE"
-          todos={todoListsDone}
-          bgColor="bg-white"
-          emptyImage="/images/big-zero-done.svg"
-          emptyText="완료한 일이 아직 없어요"
-        />
-      </div>
-    );
+export default function TotalListTab({ goalId, todoLists, todoListsDone }: TotalListTabProps) {
+  return (
+    <div className="flex flex-col space-y-4 md:space-y-4 lg:flex-row lg:space-x-8">
+      <TodoSection
+        goalId={goalId}
+        title="TO DO"
+        todos={todoLists}
+        bgColor="bg-orange-100"
+        emptyImage="/images/big-zero-todo.svg"
+        emptyText="해야할 일이 아직 없어요"
+        showActions
+      />
+      <TodoSection
+        goalId={goalId}
+        title="DONE"
+        todos={todoListsDone}
+        bgColor="bg-white"
+        emptyImage="/images/big-zero-done.svg"
+        emptyText="완료한 일이 아직 없어요"
+      />
+    </div>
+  );
 }
