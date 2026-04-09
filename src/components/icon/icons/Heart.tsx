0589cf66a31c23d94c6690a10773c3d96ea8b3b1
suwojiii <bigbanggoodz@gmail.com>
2026-04-09 17:14:22 +0900

    🎨 style: 하트 모양 변경 및 색상 추가

--- src/components/icon/icons/Heart.tsx
+++ src/components/icon/icons/Heart.tsx
@@ -15,9 +15,9 @@ export const HeartIcon = ({ filled = false, ...props }: HeartIconProps) => {
       {...props}
     >
       <path
-        d="M12 21C12 21 3 15.5 3 9C3 6.23858 5.23858 4 8 4C9.6569 4 11.1287 4.79417 12 6.02956C12.8713 4.79417 14.3431 4 16 4C18.7614 4 21 6.23858 21 9C21 15.5 12 21 12 21Z"
-        fill={filled ? '#ef4444' : 'transparent'}
-        stroke={filled ? '#ef4444' : 'currentColor'}
+        d="M12 21C12 21 3 15.5 3 9C3 6.23858 5.23858 4 8 4C9.6569 4 11.1287 4.99417 12 7.5C12.8713 4.99417 14.3431 4 16 4C18.7614 4 21 6.23858 21 9C21 15.5 12 21 12 21Z"
+        fill={filled ? 'var(--color-red-500)' : 'transparent'}
+        stroke={filled ? 'var(--color-red-500)' : 'currentColor'}
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
