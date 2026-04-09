7bd8b1e6297a73dde06f6b056a92680840fc6464
suwojiii <bigbanggoodz@gmail.com>
2026-04-09 11:38:32 +0900

    🎨 style: 헤더 스타일 변경

--- src/components/formHeader/MobileFormHeader.tsx
+++ src/components/formHeader/MobileFormHeader.tsx
@@ -24,7 +24,7 @@ export function MobileFormHeader({
   return (
     <div className="md:hidden">
       <div className="fixed top-0 right-0 left-0 z-40 flex flex-col">
-        <header className="flex min-h-16 items-center justify-between border-b border-gray-200 bg-white px-4 py-3 shadow-[0px_0px_30px_0px_rgba(0,0,0,0.05)]">
+        <header className="flex min-h-16 items-center justify-between border-b border-gray-200 bg-white px-4 py-1 shadow-[0px_0px_30px_0px_rgba(0,0,0,0.05)]">
           <div className="flex items-center gap-3">
             <SidebarTrigger />
             <span className="font-base-semibold text-gray-700">{headerTitle}</span>
