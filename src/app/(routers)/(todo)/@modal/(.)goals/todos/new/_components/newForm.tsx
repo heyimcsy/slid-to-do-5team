512699aff1e11a42dd121b3d35a815ed1f271d7c
Seunggi Jo <jojosg@nate.com>
2026-04-10 17:29:24 +0900

    fix: 링크 input 검증 정규식 수정

--- src/app/(routers)/(todo)/@modal/(.)goals/todos/new/_components/newForm.tsx
+++ src/app/(routers)/(todo)/@modal/(.)goals/todos/new/_components/newForm.tsx
@@ -54,7 +54,7 @@ const schema = z.object({
   link: z
     .string()
     .regex(
-      /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/,
+      /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z]{2,24}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/,
       '올바른 URL 형식을 입력해주세요 (예: http://www.example.com)',
     )
     .optional()
