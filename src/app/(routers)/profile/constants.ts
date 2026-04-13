be03799e3af2caf0dc90eaa306f297fe6b835179
sychoi <cinnamon1995@naver.com>
2026-04-13 15:01:42 +0900

    :sparkles: features: 하드코딩 제거 및 타입 한곳으로 분리

---
+++ src/app/(routers)/profile/constants.ts
@@ -0,0 +1,38 @@
+export const PROFILE_TEXT = {
+  TITLE: '내 정보 관리',
+  SUCCESS_MESSAGE: '저장되었습니다.',
+  ERROR_MESSAGE: '저장 중 문제가 발생했습니다.',
+  DUPLICATE_CHECK: '중복확인 해주세요',
+  SAVE: '저장하기',
+  IMAGE_EDIT: '프로필 이미지 편집',
+  EMAIL: '이메일',
+  NAME: '이름',
+  DUPLICATE: '중복확인',
+  CHECK: {
+    POSSIBLE: '사용 가능한 이름입니다.',
+    IMPOSSIBLE: '중복된 이름입니다.',
+  },
+  NAME_PLACEHOLDER: '닉네임을 입력해주세요.',
+  CHANGE_PASSWORD: '비밀번호 변경',
+  CURRENT_PASSWORD: '현재 비밀번호를 입력해주세요',
+  NEW_PASSWORD: '새 비밀번호를 입력해주세요',
+  CONFIRM_PASSWORD: '새 비밀번호를 다시 입력해주세요',
+  PASSWORD_LENGTH: '비밀번호는 8자 이상이어야 합니다',
+  REPEAT_PASSWORD: '비밀번호를 다시 입력해주세요',
+  DUPLICATE_PASSWORD: '기존 비밀번호와 새 비밀번호가 일치합니다.',
+  PASSWORD_UNSAME: '비밀번호가 일치하지 않습니다',
+  CURRENT_PASSWORD_UNSAME: '현재 비밀번호가 일치하지 않습니다',
+  NICKNAME_PLACEHOLDER: '닉네임을 입력해주세요',
+  NICKNAME_LENGTH: '20자 이하로 입력해주세요',
+};
+
+export const PROFILE_IMAGE = {
+  ALT: '유저 이미지',
+};
+
+export const CHECK_NICKNAME = 'check-nickname';
+export const EMAIL = 'email' as const;
+export const NAME = 'name' as const;
+export const CURRENT_PASSWORD = 'currentPassword' as const;
+export const NEW_PASSWORD = 'newPassword' as const;
+export const CONFIRM_PASSWORD = 'confirmPassword' as const;
