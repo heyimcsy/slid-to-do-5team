37ba8148d739ce3a93f35b9dc2387add559aa6d4
ChoboDev <gealot@gmail.com>
2026-04-15 10:02:40 +0900

    ✨ feat(TodoList): add variant-based styling and enhance TodoListSection
    - TodoList 컴포넌트에 variant prop 추가하여 스타일을 recent, completed, pending에 따라 다르게 적용
    - TodoListSection에서 variant prop을 TodoList에 전달하여 일관된 스타일 유지
    - 빈 할 일 리스트에 대한 이미지 및 텍스트 스타일 개선

--- src/app/(routers)/(dashboard)/dashboard/_components/TodoListSection.tsx
+++ src/app/(routers)/(dashboard)/dashboard/_components/TodoListSection.tsx
@@ -1,8 +1,9 @@
 import type { TodoWithFavorites } from '@/api/todos';
 
-import TodoList from '@/app/(routers)/(todo)/goals/[goalId]/_components/TodoList';
-import { cn } from '@/lib';
 import Image from 'next/image';
+import { cn } from '@/lib';
+
+import TodoList from './TodoList';
 
 export function TodoListSection({
   title,
@@ -20,7 +21,9 @@ export function TodoListSection({
     <section
       className={cn(
         'flex-1 rounded-2xl p-4 md:p-4 lg:p-6',
-        variant === 'completed' ? '' : 'bg-orange-100',
+        variant === 'completed'
+          ? ''
+          : 'bg-orange-100 text-black dark:bg-orange-300 dark:text-black',
       )}
     >
       <h2
@@ -31,11 +34,17 @@ export function TodoListSection({
       <div className="mt-4 space-y-2">
         {items.length === 0 ? (
           <div className="flex h-full flex-col items-center justify-center">
-            <Image src={emptyImage} alt={`${emptyText} 할 일 없음`} width={130} height={140} />
-            <p className="text-sm text-gray-500">{`${emptyText} 할 일이 없어요`}</p>
+            <Image
+              src={emptyImage}
+              alt={`${emptyText} 할 일 없음`}
+              width={130}
+              height={140}
+              className="h-21.25 w-20 object-contain md:h-35 md:w-32.5 lg:h-35 lg:w-32.5"
+            />
+            <p className="text-sm text-gray-500 dark:text-black">{`${emptyText} 할 일이 없어요`}</p>
           </div>
         ) : (
-          items.map((item) => <TodoList key={item.id} {...item} />)
+          items.map((item) => <TodoList key={item.id} {...item} variant={variant} />)
         )}
       </div>
     </section>
